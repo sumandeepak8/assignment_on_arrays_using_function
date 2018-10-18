6e0842ddefba923113f98584d812341e5491494e
Suman Rajput <sumanraj@in-sumanraj.local>
2018-10-18 09:27:20 +0530

    tried cloning repo and solving the problem of pushing

--- library_file.js
+++ library_file.js
@@ -1,63 +1,65 @@
-//to separate the odd numbers out of an array.
-const separateOddNumbers = function(numbers){
-  let indexOfOddNumber = 0;
-  let oddNumbers =[];
-  for(let index = 0; index < numbers.length; index++){
-    if(numbers[index]%2 !=0 ){
-      oddNumbers[indexOfOddNumber] = +numbers[index];
-      indexOfOddNumber += 1;
+const isOdd = function(numbers){
+  let oddNumbers = [];
+  for(number of numbers){
+    if(number%2 != 0){
+      oddNumbers.push(number);
     }
   }
   return oddNumbers;
 }
 
-//to separate even numbers.
-const separateEvenNumbers = function(numbers){
-  let indexOfEvenNumber = 0;
-  let evenNumbers =[];
-  for(let index = 0; index < numbers.length; index++){
-    if(numbers[index]%2 ==0 ){
-      evenNumbers[indexOfEvenNumber] = +numbers[index];
-      indexOfEvenNumber += 1;
+
+// to separate the odd numbers out of an array.
+
+const extractOddNumbers = function(numbers){
+  return isOdd(numbers);
+}
+
+const isEven = function(numbers) {
+   let evenNumbers = [];
+  for(number of numbers){
+    if(number%2 == 0){
+      evenNumbers.push(number);
     }
   }
   return evenNumbers;
 }
 
-//to find the sum of number.
-const sumOfNumbersOfList = function(numbersList){
+
+// to separate even numbers.
+
+const extractEvenNumbers = function(numbers){
+  return isEven(numbers);
+}
+
+// to find the sum of number.
+const calculateSum = function(numbers){
   let sum = 0;
-  for(let indexOfListNumbers = 0; indexOfListNumbers < numbersList.length; indexOfListNumbers++){
-    sum = sum+numbersList[indexOfListNumbers];
+  for(number of numbers){
+    sum = sum+number;
   }
   return sum;
 }
 
 // to reverse the given array.
-const reverseArray = function(numberArray){
-  let reverseNumberArray = [];
-  for(let index = 0; index <= numberArray.length-1; index++){
-    reverseNumberArray.unshift(numberArray[index]);
-  }
-  return reverseNumberArray;
+const reverseArray = function(numbers){
+  return numbers.reverse();
 }
 
-//to select every second element.
-const selectEverySecondNumber = function(numberList){
-  let everySecondNumberList = [];
-  let indexForEverySecondNumberList = 0;
-  for(let index = 0; index < numberList.length; index++){
-    if(index%2 ==0){
-      everySecondNumberList[indexForEverySecondNumberList] = numberList[index];
-      indexForEverySecondNumberList++;
+// to  every second element.
+const everySecondNumber = function(numbers){
+  let numberList = [];
+  for(let index = 0; index < numbers.length; index++){
+    if(index%2 == 0){
+      numberList.push(numbers[index]);
     }
   }
-  return everySecondNumberList;
+  return numberList;
 }
 
 
 // to generate reverse fibonacci.
-const genReverseFibonacciSeries = function(number){
+const reverseFibonacci = function(number){
   let fiboNumbers = [];
   fiboNumbers[1] = 0;
   fiboNumbers[0] = 1;
@@ -91,34 +93,40 @@ const findLowestNumber = function(numbers){
 }
 
 //test for average of the numbers of list.
-const averageOfNumbers = function(sumOfNumbersOfList,numbers){
-  let average = sumOfNumbersOfList(numbers)/numbers.length;
+const average = function(numbers){
+  let average = calculateSum(numbers)/numbers.length;
   return average;
 }
 
 
+
 //to calculate the length of array elements.
-const mappingLength = function(names){
-  namesLength = [];
-  for(let index = 0; index<names.length; index++){
-    namesLength[index] = names[index].length;
+const mappingLength = function(source){
+  let elementLengths = [];
+  for(element of source){
+    elementLengths.push(element.length);
   }
-  return namesLength;
+  return elementLengths;
 }
 
 
-//to count the odd numbers.
-const countNumbers = function( reference,numbers){
-  let count = reference(numbers).length;
+// to count the odd numbers.
+const countEvenNumbers = function(numbers){
+  let count = extractEvenNumbers(numbers).length;
   return count;
 }
 
-//to count the numbers the are above to a certain threshold in an array.
+// to count the even numbers.
+const countOddNumbers = function(numbers){
+  let count = extractOddNumbers(numbers).length;
+  return count;
+}
+
+// to count the numbers the are above to a certain threshold in an array.
 const countNumberAbove = function(numbers,number){
-  let length = numbers.length;
   let count = 0;
-  for(let index = 0; index<length; index++){
-    if(numbers[index] > number){
+  for(num of numbers){
+    if(num > number){
       count++;
     }
   }
@@ -126,41 +134,185 @@ const countNumberAbove = function(numbers,number){
 }
 
 //to count the numbers that are below to a certain threshold in an array.
-const countNumberBelow = function(countNumberAbove,numbers,number){
+const countNumberBelow = function(numbers,number){
   let  count = numbers.length-countNumberAbove(numbers,number)-1;
   return count;
 }
 
 //to find a reversed version of an array without changing the original array.
-const genReverseArray = function(reverseArray,numbers){
-  return reverseArray(numbers);
+const genReverseArray = function(numbers){
+  let reverse = [];
+  for(number of numbers){
+    reverse.push(number);
+  }
+  return reverseArray(reverse);
 }
 
 //to find the first position of a number in a given array.
-const findFirstPosition = function(numbers,number){
-  let positionOfNumber;
+const findFirstIndex = function(numbers,number){
+  let indexOfNumber;
   for(let index = 0; index < numbers.length; index++){
     if(numbers[index] == number){
-      positionOfNumber = index;
+      indexOfNumber = index;
       numbers.length = 0;
     }
   }
-  return positionOfNumber;
+  return indexOfNumber;
 }
 
-exports.separateOddNumbers= separateOddNumbers;
-exports.separateEvenNumbers = separateEvenNumbers;
-exports.sum = sumOfNumbersOfList;
+//to check if the array is in ascending order.
+const toCheckOrder = function(numbers){
+  let index = 0;
+  let numToCompare = numbers[index];
+  for(index = 1; (index<numbers.length) && (numToCompare<=numbers[index]); index++){
+    numToCompare = numbers[index];
+  }
+  return (index == numbers.length);
+}
+
+//to check the descending order of array.
+const checkDescendingOrder = function(numbers){
+  let index = 0;
+  let numToCompare = numbers[index];
+  for(index = 1; (index<numbers.length) && (numToCompare>=numbers[index]); index++){
+    numToCompare = numbers[index];
+  }
+  return (index == numbers.length);
+}
+
+//to extract digits of a number in array.
+const extractDigits = function(number){
+  let digits = [];
+  number = number.toString().split("");
+  let length = number.length;
+  for(let index = 0; index < length; index++){
+    digits[index] = parseInt(number.shift());
+  }
+  return digits;
+}
+
+//to unique the elements of array.
+const unique = function(numbers){ 
+  let uniqueNumbers = [];
+  let index = 0;
+  uniqueNumbers[index] = numbers[index]
+  for(index = 1; index < numbers.length; index++){
+    if(!uniqueNumbers.includes(numbers[index]))
+      uniqueNumbers.push(numbers[index]);
+  }
+  return uniqueNumbers;
+}
+
+
+//to get union of two array.
+const unite = function(first,second){
+ let union = unique(first.concat(second));
+  return union;
+}
+
+
+//to get the intersection of two arrays.
+const getIntersection = function(firstNumbers,secondNumbers){
+  let commonNumbers = [];
+  firstNumbers = unique(firstNumbers);
+  secondNumbers = unique(secondNumbers);
+  for( let index = 0; (index < secondNumbers.length || index < firstNumbers.length); index++){
+    if(firstNumbers.includes(secondNumbers[index])){
+      commonNumbers.push(secondNumbers[index]);
+    }
+  }
+  return commonNumbers;
+}
+
+//first array contains and second doesn't.
+const difference = function(first,second){
+  first = unique(first);
+  second = unique(second);
+  let diff = [];
+  for(number of first){
+    if( !second.includes(number)){
+      diff.push(number);
+    }
+  }
+  return diff;
+}
+
+//to find if the second array is proper subset of first 
+const isSubset = function(array,subsetCandidate){
+  array = unique(array);
+  subsetCandidate = unique(subsetCandidate);
+  let count = 0;
+  for( element of subsetCandidate){
+    if(array.includes(subsetCandidate[index])){
+      count += 1;
+    }
+  }
+  return (count == subsetCandidate.length);
+}
+
+//to insert elements from two arrays.
+const zipArrays = function(first,second){
+  let zipNumbers =[];
+  for(let index=0; (index<first.length && index<second.length); index++){
+    zipNumbers.push([first[index],second[index]]);
+  }
+  return zipNumbers;
+}
+
+//to rotate the given array.
+const rotate = function(array,times){
+  let element;
+  for(let index=0; index<times; index++){
+    element = array.shift();
+    array.push(element);
+  }
+  return array;
+}
+
+//to create a partitioned array.
+const partition = function(array,number) {
+  let lessNumber = [];
+  let remaining = [];
+  let length = array.length;
+  for(let index=0; index<length; index++){
+    if(array[index]>number){
+      remaining.push(array[index]);
+      continue;
+    }
+    lessNumber.push(array[index]);
+  }
+  array = [];
+  array.push(lessNumber);
+  array.push(remaining);
+  return array; 
+}
+
+
+
+exports.extractOddNumbers= extractOddNumbers;
+exports.extractEvenNumbers = extractEvenNumbers;
+exports.sum = calculateSum;
 exports.reverse = reverseArray;
-exports.everySecondNumber = selectEverySecondNumber;
-exports.reverseFibonacci = genReverseFibonacciSeries;
+exports.number = everySecondNumber;
+exports.reverseFibonacci = reverseFibonacci;
 exports.greatestNum = findGreatestNumber;
 exports.lowestNum = findLowestNumber;
-exports.average = averageOfNumbers;
+exports.average = average;
 exports.mapLength = mappingLength;
-exports.countNumbers = countNumbers;
+exports.countEvenNumbers = countEvenNumbers;
+exports.countOddNumbers = countOddNumbers;
 exports.countNumberAbove = countNumberAbove;
 exports.countNumberBelow = countNumberBelow;
-exports.genReverseArray = genReverseArray;
-exports.position = findFirstPosition;
-
+exports.genReverse = genReverseArray;
+exports.position = findFirstIndex;
+exports.ascendingOrder = toCheckOrder;
+exports.descendingOrder = checkDescendingOrder;
+exports.digits = extractDigits;
+exports.unique = unique;
+exports.union = unite;
+exports.common = getIntersection;
+exports.diff = difference;
+exports.isSubset = isSubset;
+exports.zipArrays = zipArrays;
+exports.rotate = rotate;
+exports.partition = partition;
--- test_library_file.js
+++ test_library_file.js
@@ -1,14 +1,14 @@
-const lib = require('./library_file.js');
 const assert = require('assert');
+const lib = require('./library_file.js');
 
 
 //test to separate odd numbers.
-const oddNumbers = lib.separateOddNumbers;
+const oddNumbers = lib.extractOddNumbers;
 assert.deepStrictEqual(oddNumbers([1,2,3,4,5,6]),[1,3,5]);
 
 
 //test of separate even number program.
-const evenNumbers = lib.separateEvenNumbers;
+const evenNumbers = lib.extractEvenNumbers;
 assert.deepStrictEqual(evenNumbers([0,1,2,3,4,5,6,7,8]),[0,2,4,6,8]);
 
 
@@ -23,8 +23,8 @@ assert.deepStrictEqual(reverse([2,4,6,8]),[8,6,4,2]);
 
 
 //to find every second element.
-const everySecondNumber = lib.everySecondNumber;
-assert.deepStrictEqual(everySecondNumber([1,2,3,4,5,6,7]),[1,3,5,7]);
+const number = lib.number;
+assert.deepStrictEqual(number([1,2,3,4,5,6,7]),[1,3,5,7]);
 
 
 //test for reverse fibonacci but input should be greater than 1.
@@ -44,7 +44,8 @@ assert.deepEqual(lowestNum([5,6,5,4,8,2,7,8]),2);
 
 //test for average of numbers.
 const average = lib.average;
-assert.deepEqual(average(sum,[2,6,6,4,2]),4);
+assert.deepEqual(average([2,6,6,4,2]),4);
+assert.deepEqual(average([2,6,0,2,2,0]),2);
 
 
 //test for mapping length.
@@ -53,31 +54,106 @@ assert.deepEqual(mapLength(["suman","deepak","1"]),[5,6,1]);
 
 
 //test for count odd numbers.
-const countNumbers = lib.countNumbers;
-assert.deepEqual(countNumbers(oddNumbers,[1,2,3,4,5,6,7]),4);
+const countOddNumbers = lib.countOddNumbers;
+assert.deepEqual(countOddNumbers([1,2,3,4,5,6,7]),4);
 
 
 //test for count even numbers.
-assert.deepEqual(countNumbers(evenNumbers,[1,2,34,4,5,6,7]),4);
+const {countEvenNumbers} = lib;
+assert.deepEqual(countEvenNumbers([1,2,34,4,5,6,7]),4);
 
 
 //test for countNumberAbove.
 const countNumberAbove = lib.countNumberAbove;
 assert.deepEqual(countNumberAbove([1,2,3,4,5,6,7,8],3),5);
+assert.deepEqual(countNumberAbove([6,9,7,3,5,1,7,8],3),6);
 
 
 //test for countNumberBelow.
 const countNumberBelow = lib.countNumberBelow;
-assert.deepStrictEqual(countNumberBelow(countNumberAbove,[1,2,3,4,5,6,7],6),5);
+assert.deepStrictEqual(countNumberBelow([1,2,3,4,5,6,7],6),5);
+assert.deepStrictEqual(countNumberBelow([1,2,4,5,6,7],4),2);
 
+/*
 
 //test for reversed array without changing the given array.
-const reversedArray = lib.genReverseArray;
-assert.deepStrictEqual(reversedArray(reverse,[1,2,3,4,5,6,7,8]),[8,7,6,5,4,3,2,1]);
-assert.deepStrictEqual(reversedArray(reverse,[1,2,3,"suman",5,6,7,8]),[8,7,6,5,"suman",3,2,1]);
-
+const reversedArray = lib.genReverse;
+assert.deepStrictEqual(reversedArray([1,2,3,4,5,6,7,8]),[8,7,6,5,4,3,2,1]);
+assert.deepStrictEqual(reversedArray([1,2,3,"suman",5,6,7,8]),[8,7,6,5,"suman",3,2,1]);
 
 //to find the first position of specified number.
 const position = lib.position;
 assert.deepStrictEqual(position([3,4,5,7,1,7,5],1),4);
+assert.deepStrictEqual(position([7,4,6,9,1,2,5],9),3);
 
+
+//test to check the ascending order of array.
+const ascendingOrder = lib.ascendingOrder;
+assert.deepStrictEqual(ascendingOrder([1,2,3,4,5,6]),true);
+assert.deepStrictEqual(ascendingOrder([1,2,7,4,5,6]),false);
+assert.deepStrictEqual(ascendingOrder([1,2,3,5,5,6]),true);
+
+//test to check the descending order of array.
+const descendingOrder = lib.descendingOrder;
+assert.deepStrictEqual(descendingOrder([1,2,3,4,5,6]),false);
+assert.deepStrictEqual(descendingOrder([8,5,4,2]),true);
+assert.deepStrictEqual(descendingOrder([1,2,3,5,5,6]),false);
+
+
+//test to extract digits from a number.
+const digits = lib.digits;
+assert.deepStrictEqual(digits([20981]),[2,0,9,8,1]);
+assert.deepStrictEqual(digits([32987]),[3,2,9,8,7]);
+
+
+//test to remove duplicacy from an array.
+const unique = lib.unique;
+assert.deepStrictEqual(unique([1,2,3,4,3,5,4,2,6]),[1,2,3,4,5,6]);
+assert.deepStrictEqual(unique([7,2,5,7,3,5,4,2,6]),[7,2,5,3,4,6]);
+assert.deepStrictEqual(unique([7,2,5,7,3,"suman",4,2,6]),[7,2,5,3,"suman",4,6]);
+
+
+//test for union of arrays.
+const union = lib.union;
+assert.deepStrictEqual(union([1,4,5,6,1],["hello",2,3,1,0,1],),[1,4,5,6,"hello",2,3,0]);
+assert.deepStrictEqual(union([0,4,5,5,1],["hello",2,3,1,0,1],),[0,4,5,1,"hello",2,3]);
+
+
+//test to get intersection of two arrays.
+const common = lib.common;
+assert.deepStrictEqual(common([1,2,3,2,3],[0,,4,2,,1],),[2,1]);
+assert.deepStrictEqual(common([1,2,3,2,3,0],[0,3,4,2,,1],),[0,3,2,1]);
+
+
+//test to get diffence between two array.
+const diff = lib.diff;
+assert.deepStrictEqual(diff([2,4,6,8],[0,6],),[2,4,8]);
+assert.deepStrictEqual(diff([0,2,4,6,1,8],[0,6,,,,,,,,],),[2,4,1,8]);
+assert.deepStrictEqual(diff([0,2,," ",4,6,1,8],[0,6],),[2,undefined," ",4,1,8]);
+
+//test if the second array is proper subset of first array.
+const isSubset = lib.isSubset;
+assert.deepStrictEqual(isSubset([1,2,3,49],[1,49],),true);
+assert.deepStrictEqual(isSubset([1,2,"hello",40],[1,49],),false);
+assert.deepStrictEqual(isSubset([1,"hello",0,40],[1,"hello",40],),true);
+
+
+// test to get new array formed by two array.
+const zipArrays = lib.zipArrays;
+assert.deepStrictEqual(zipArrays([2,5,3],[2,5,3]),[[2,2],[5,5],[3,3]]);
+assert.deepStrictEqual(zipArrays([2,5,3],[2,5]),[[2,2],[5,5]]);
+assert.deepStrictEqual(zipArrays([2,"hello",3],[2,5,]),[[2,2],["hello",5]]);
+
+
+
+// test for rotate array.
+const rotateArray = lib.rotateArray;
+assert.deepStrictEqual(rotateArray([3,6,7,2,4],2),[7,2,4,3,6]);
+assert.deepStrictEqual(rotateArray([1,2,3,4,5],2),[3,4,5,1,2]);
+
+
+//test for partioned array.
+const {partition } =lib 
+assert.deepStrictEqual(partition([2,3,5,7,1,4],3),[[2,3,1],[5,7,4]]);
+assert.deepStrictEqual(partition([8,3,5,6,1,4],5),[[3,5,1,4],[8,6]]);
+*/
